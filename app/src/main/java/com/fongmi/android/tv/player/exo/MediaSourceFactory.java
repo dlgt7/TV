f977fd707f4bc9ecdf0c9e4227645cdfd1438b79
FongMi <b8892778@gmail.com>
2026-08-27 19:42:04 +0800

    Harden filesystem operations
    (cherry picked from commit b3b4c24787ef789ed243bf89973462667ca6544e)

--- app/src/main/java/com/fongmi/android/tv/player/exo/MediaSourceFactory.java
+++ app/src/main/java/com/fongmi/android/tv/player/exo/MediaSourceFactory.java
@@ -24,7 +24,6 @@ import androidx.media3.extractor.ts.TsExtractor;
 
 import com.fongmi.android.tv.App;
 import com.fongmi.android.tv.setting.PreloadSetting;
-import com.fongmi.android.tv.utils.FileUtil;
 import com.github.catvod.net.OkHttp;
 import com.github.catvod.utils.Path;
 
@@ -65,8 +64,8 @@ public class MediaSourceFactory implements MediaSource.Factory {
     }
 
     private static long getMaxCacheSize(File dir) {
-        long usedBytes = FileUtil.getDirectorySize(dir);
-        long availableBytes = Math.max(0, FileUtil.getAvailableStorageSpace(dir));
+        long usedBytes = Path.size(dir);
+        long availableBytes = Math.max(0, Path.available(dir));
         long storageBudget = (usedBytes + availableBytes) * CACHE_SPACE_PERCENT / 100;
         return Math.min(PreloadSetting.getPreloadSizeBytes(), storageBudget);
     }
