c3761697dcb92b96ebd287022d58c87d5d085f35
dlgt7 <102397160+dlgt7@users.noreply.github.com>
2026-09-19 07:25:31 +0800

    Add files via upload
    Signed-off-by: dlgt7 <102397160+dlgt7@users.noreply.github.com>

--- app/src/stubs/java/androidx/media3/exoplayer/text/SecondaryTextOutput.java
+++ app/src/stubs/java/androidx/media3/exoplayer/text/SecondaryTextOutput.java
@@ -1,1 +1,9 @@
-1
+package androidx.media3.exoplayer.text;
+
+public final class SecondaryTextOutput implements TextOutput {
+
+    public SecondaryTextOutput() {}
+
+    @Override
+    public void onCues(androidx.media3.common.text.CueGroup cueGroup) {}
+}
