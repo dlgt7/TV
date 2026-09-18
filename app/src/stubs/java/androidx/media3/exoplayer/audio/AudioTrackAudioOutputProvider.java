c6551795387cb9d59594c1a5e4bef68c68431113
dlgt7 <102397160+dlgt7@users.noreply.github.com>
2026-09-19 07:21:40 +0800

    Add files via upload
    Signed-off-by: dlgt7 <102397160+dlgt7@users.noreply.github.com>

--- app/src/stubs/java/androidx/media3/exoplayer/audio/AudioTrackAudioOutputProvider.java
+++ app/src/stubs/java/androidx/media3/exoplayer/audio/AudioTrackAudioOutputProvider.java
@@ -1,1 +1,16 @@
-1
+package androidx.media3.exoplayer.audio;
+
+import androidx.annotation.Nullable;
+
+public final class AudioTrackAudioOutputProvider {
+
+    public static final class Builder {
+        @Nullable
+        private Object audioProcessor;
+
+        public Builder(@Nullable Object audioProcessor) { this.audioProcessor = audioProcessor; }
+        public AudioTrackAudioOutputProvider build() { return new AudioTrackAudioOutputProvider(); }
+    }
+
+    private AudioTrackAudioOutputProvider() {}
+}
