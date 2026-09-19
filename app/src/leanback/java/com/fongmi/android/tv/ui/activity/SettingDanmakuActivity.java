a0e405dc2ca08b10ec685274fb8d247d27e93e9c
dlgt7 <102397160+dlgt7@users.noreply.github.com>
2026-09-19 11:28:35 +0800

    Add files via upload
    Signed-off-by: dlgt7 <102397160+dlgt7@users.noreply.github.com>

--- app/src/leanback/java/com/fongmi/android/tv/ui/activity/SettingDanmakuActivity.java
+++ app/src/leanback/java/com/fongmi/android/tv/ui/activity/SettingDanmakuActivity.java
@@ -15,12 +15,10 @@ import com.fongmi.android.tv.setting.DanmakuSetting;
 import com.fongmi.android.tv.setting.Setting;
 import com.fongmi.android.tv.ui.base.BaseActivity;
 import com.fongmi.android.tv.ui.dialog.DanmakuApiDialog;
-import com.fongmi.android.tv.ui.dialog.DanmuAlphaDialog;
-import com.fongmi.android.tv.ui.dialog.DanmuLineDialog;
-import com.fongmi.android.tv.ui.dialog.DanmuSizeDialog;
+import com.fongmi.android.tv.ui.dialog.DanmakuSettingDialog;
 import com.fongmi.android.tv.utils.ResUtil;
 
-public class SettingDanmakuActivity extends BaseActivity implements DanmakuListener, DanmuSizeDialog.Listener, DanmuAlphaDialog.Listener, DanmuLineDialog.Listener {
+public class SettingDanmakuActivity extends BaseActivity implements DanmakuListener {
 
     private ActivitySettingDanmakuBinding mBinding;
     private String[] danmuSpeed;
@@ -45,9 +43,7 @@ public class SettingDanmakuActivity extends BaseActivity implements DanmakuListe
         mBinding.danmakuAutoText.setText(Setting.getSwitch(DanmakuSetting.isAuto()));
         mBinding.danmakuLoadText.setText(Setting.getSwitch(DanmakuSetting.isLoad()));
         mBinding.danmakuSpiderText.setText(Setting.getSwitch(DanmakuSetting.isSpiderFirst()));
-        mBinding.danmuSizeText.setText(String.valueOf(DanmakuSetting.getTextScale()));
-        mBinding.danmuAlphaText.setText(String.valueOf((int) (DanmakuSetting.getTransparency() * 100)));
-        mBinding.danmuLineText.setText(String.valueOf(DanmakuSetting.getMaxScrollLines()));
+
         mBinding.danmuSpeedText.setText((danmuSpeed = ResUtil.getStringArray(R.array.select_danmu_speed))[Setting.getDanmuSpeed()]);
         updateApiVisibility();
     }
@@ -58,9 +54,7 @@ public class SettingDanmakuActivity extends BaseActivity implements DanmakuListe
         mBinding.danmakuAuto.setOnClickListener(this::setDanmakuAuto);
         mBinding.danmakuLoad.setOnClickListener(this::setDanmakuLoad);
         mBinding.danmakuSpider.setOnClickListener(this::setDanmakuSpider);
-        mBinding.danmuSize.setOnClickListener(this::onDanmuSize);
-        mBinding.danmuAlpha.setOnClickListener(this::onDanmuAlpha);
-        mBinding.danmuLine.setOnClickListener(this::onDanmuLine);
+        mBinding.danmakuStyle.setOnClickListener(this::onDanmakuStyle);
         mBinding.danmuSpeed.setOnClickListener(this::setDanmuSpeed);
     }
 
@@ -109,34 +103,8 @@ public class SettingDanmakuActivity extends BaseActivity implements DanmakuListe
         mBinding.danmakuSpiderText.setText(Setting.getSwitch(DanmakuSetting.isSpiderFirst()));
     }
 
-    private void onDanmuSize(View view) {
-        DanmuSizeDialog.create(this).show();
-    }
-
-    @Override
-    public void setDanmuSize(float size) {
-        DanmakuSetting.putTextScale(size);
-        mBinding.danmuSizeText.setText(String.valueOf(size));
-    }
-
-    private void onDanmuAlpha(View view) {
-        DanmuAlphaDialog.create(this).show();
-    }
-
-    @Override
-    public void setDanmuAlpha(int alpha) {
-        DanmakuSetting.putTransparency(alpha / 100.0f);
-        mBinding.danmuAlphaText.setText(String.valueOf(alpha));
-    }
-
-    private void onDanmuLine(View view) {
-        DanmuLineDialog.create(this).show();
-    }
-
-    @Override
-    public void setDanmuLine(int line) {
-        DanmakuSetting.putMaxScrollLines(line);
-        mBinding.danmuLineText.setText(String.valueOf(line));
+    private void onDanmakuStyle(View view) {
+        DanmakuSettingDialog.create().show(this);
     }
 
     private void setDanmuSpeed(View view) {
